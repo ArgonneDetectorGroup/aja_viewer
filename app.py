73c437e5d03a83e2c6950c140a5e621d2e8306fb
Faustin Carter <faustin.carter@gmail.com>
2017-05-05 13:19:16 -0500

    add option for interactive vs non-interactive plots

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+from __future__ import division
+
 import os
 import sqlite3
 import flask
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly as ply
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.figure
+from matplotlib.figure import Figure
 from io import BytesIO
 
 app = flask.Flask(__name__)
@@ -23,45 +27,97 @@ def get_db():
         db = flask.g._database = sqlite3.connect(DATABASE)
     return db
 
+def gen_timeseries(db, table_name, recipe):
+    query = """select * from {} where
+            Recipe_Steps like ?""".format(table_name)
+
+    df = pd.read_sql_query(query, db, params=(recipe,), index_col='Date_Time', parse_dates=['Date_Time'])
+
+    df = df[df.columns[(df != 0).any()]]
+
+    keys_to_plot = df.select_dtypes(['number']).drop(['Layer_#', 'Wafer_#_Loaded'], axis=1).columns.tolist()
+
+    num_cols = 3
+    num_rows = np.ceil((len(keys_to_plot)+1)/num_cols)
+
+    axes = []
+
+    fig = Figure(figsize=(9, 9))
+    canvas = FigureCanvas(fig)
+
+    for ix, key in enumerate(keys_to_plot):
+        ax = fig.add_subplot(num_rows, num_cols, ix+1)
+        ax.set_title(key)
+        axes.append(ax)
+
+    for key, val in df.groupby(('Layer_#', 'Logfile_Path')):
+        #print(val.index[0])
+        filtered_val = val.select_dtypes(['number']).drop(['Layer_#', 'Wafer_#_Loaded'], axis=1)
+
+        for ix, key in enumerate(keys_to_plot):
+
+            yvals = filtered_val[key].values
+            xvals = (filtered_val.index-filtered_val.index[0]).total_seconds()
+            axes[ix].plot(xvals, yvals, label=str(filtered_val.index[0]))
+
+    fig.canvas.draw()
+    for ax in axes:
+        ax.set_xticklabels(labels=ax.get_xmajorticklabels(), rotation=45)
+        ax.set_xlabel('Time (s)')
+
+    fig.tight_layout()
+    fig.savefig('figure.png')
+
+    return fig
+
 @app.route('/')
 def index():
     df = pd.read_sql_query("SELECT name FROM sqlite_master WHERE type='table'", get_db())
     table_names = df['name'].values.tolist()
     return flask.render_template('index.html', table_names = table_names)
 
+@app.route('/static_plot', methods=['GET', 'POST'])
+def gen_static_plot():
+    table_name = flask.request.args['table_name']
+    recipe = flask.request.args['recipe']
+
+    fig = gen_timeseries(get_db(), table_name, recipe)
+
+    output = BytesIO()
+    # canvas.print_png(output)
+    # response=flask.make_response(output.getvalue())
+    # response.headers['Content-Type'] = 'image/png'
+
+    fig.savefig(output)
+    output.seek(0)
+    return flask.send_file(output, mimetype='image/png')
+
+    return response
 
 @app.route('/recipe_plots', methods=['GET', 'POST'])
 def show_plot():
     table_name = flask.request.form['table_name']
     recipe = flask.request.form['submit_plt']
+    interactive = flask.request.form['interactive']
 
-    query = """select * from {} where
-            Recipe_Steps like ?""".format(table_name)
+    if interactive == 'True':
 
-    df = pd.read_sql_query(query, get_db(), params=(recipe,))
+        fig = gen_timeseries(get_db(), table_name, recipe)
 
-    fig, ax = plt.subplots(1)
+        #Wrap in fancy interactive plotly div
+        output = ply.offline.plot_mpl(fig, output_type='div', show_link="False",include_plotlyjs="False")
 
-    for key, val in df.groupby(('Layer_#', 'Logfile_Path')):
-        val.plot(ax=ax, legend=False)
+        return flask.render_template('show_plots.html',
+                                    interactive=True,
+                                    recipe_name = recipe,
+                                    table_name=table_name,
+                                    plot_output=flask.Markup(output))
 
-    #ax.set_title('Recipe summary for: '+recipe)
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Some arbitrary thing')
-
-    fig.set_size_inches(8, 6)
-
-    #output = BytesIO()
-    # fig.savefig(output)
-    # output.seek(0)
-    #return flask.send_file(output, mimetype='image/png')
-
-    output = ply.offline.plot_mpl(fig, output_type='div', show_link="False",include_plotlyjs="False")
-
-    return flask.render_template('show_plots.html',
-                                recipe_name = recipe,
-                                table_name=table_name,
-                                plot_output=flask.Markup(output))
+    else:
+        return flask.render_template('show_plots.html',
+                                    interactive=False,
+                                    recipe_name = recipe,
+                                    table_name=table_name)
 
 @app.route('/recipe_frequency', methods=['GET', 'POST'])
 def calc_recipe_frequency():
